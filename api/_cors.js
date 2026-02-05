628cb4c6b583714fbb4bcc59437cbec5bbe7be67
manuel2554 <jmsquien@gmail.com>
2026-02-05 12:41:49 -0400

    Update _cors.js
    fgh

--- api/_cors.js
+++ api/_cors.js
@@ -1,15 +1,15 @@
 export function cors(req, res) {
-  // Permite tu GitHub Pages (más seguro que "*")
+  // SOLO permite tu GitHub Pages
   res.setHeader("Access-Control-Allow-Origin", "https://manuel2554.github.io");
   res.setHeader("Vary", "Origin");
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Preflight
+  // Respuesta al preflight (OPTIONS)
   if (req.method === "OPTIONS") {
     res.status(204).end();
-    return true; // ya respondimos
+    return true;
   }
   return false;
 }
